592748b68c2c99f8cb00e17abe92e50cd9683fbf
shienynursyifa <shienynursyifa@gmail.com>
2021-10-28 10:32:39 +0800

    ulang  kaji array

---
+++ table.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table</title>
+</head>
+<body>
+    
+    <!-- colspan --><center>
+			<table border="1" width="50%" cellpadding="10">
+				<tr bgcolor="orange">
+					<th>PROGRAM</th>
+					<th>KELAYAKAN</th>
+				</tr>
+				<tr bgcolor="yellow">
+					<td colspan="2">POLITEKNIK UNGKU OMAR</td>
+				</tr>
+				<tr>
+					<td>Sarjana Muda Teknologi Kejuruteraan Awam Dengan Kepujian</td>
+					<td>STPM</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+				</tr>
+                <tr>
+					<td></td>
+					<td></td>
+				</tr>
+                
+                <tr bgcolor="yellow">
+					<td colspan="2">POLITEKNIK SULTAN HAJI AHAMAD SHAH</td>
+				</tr>
+				<tr>
+					<td>Diploma Kejuruteraan Awam</td>
+					<td>STPM<p>Sijil Kolej Komuniti</p></td>
+				</tr>
+				<tr>
+					<td>Dipoma Seni Bina</td>
+					<td>STPM<p>Sijil Kolej Komuniti</p></td>
+				</tr>
+                <tr>
+					<td>Diploma Geomatik</td>
+					<td>STPM<p>Sijil Kolej Komuniti</p></td>
+				</tr>
+				</table></center>
+
+</body>
+</html>
